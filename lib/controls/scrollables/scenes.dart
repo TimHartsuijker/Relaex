33e2feb0974c19e1538f7809b835a3e3187cc96e
Tim Hartsuiker <creeperhertog@gmail.com>
2021-11-15 11:18:02 +0100

    code is gecomment

--- lib/controls/scrollables/scenes.dart
+++ lib/controls/scrollables/scenes.dart
@@ -10,19 +10,23 @@ class Scenes extends StatelessWidget {
       builder: (BuildContext context, BoxConstraints constraints) {
         return Row(
           children: <Widget>[
+            // wit ruimte
             SizedBox(
               height: 955,
               width: 375.0,
               child: Padding(
                 padding: const EdgeInsets.only(top: 100.0),
+                // scrollbar scenes
                 child: Scrollbar(
                   controller: _firstController,
                   child: ListView.builder(
                       controller: _firstController,
                       itemCount: 11,
                       itemBuilder: (BuildContext context, int index) {
+                        // return wat elke rij uit de scrollbar nodig heeft
                         return Padding(
                           padding: const EdgeInsets.all(25.0),
+                          // naam scene
                           child: Text(
                             'Scene $index',
                             style: const TextStyle(
