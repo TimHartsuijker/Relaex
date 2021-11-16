b219f16ec97954f6c44dc822898eaf3652312a1f
Sven Vos <sven.vos@omnilan.eu>
2021-11-16 09:52:46 +0100

    Code cleanup

--- lib/controls/scrollables/modules.dart
+++ lib/controls/scrollables/modules.dart
@@ -1,72 +1,80 @@
 import 'package:flutter/material.dart';
 
-class Modules extends StatefulWidget {
+class Modules extends StatelessWidget {
   const Modules({Key? key}) : super(key: key);
 
   @override
-  State<Modules> createState() => _ModulesState();
-}
-
-class _ModulesState extends State<Modules> {
-  final ScrollController _firstController = ScrollController();
-
-  List<bool> isActive = [];
-
-  @override
   Widget build(BuildContext context) {
-
     return LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
         return Row(
-          children: [
+          children: const [
             // wit ruimte
             SizedBox(
               height: 1100,
               width: 330.0,
               child: Padding(
-                padding: const EdgeInsets.only(top: 100.0),
-                // scrollbar modules
-                child: Scrollbar(
-                  controller: _firstController,
-                  child: ListView.builder(
-                    controller: _firstController,
-                    itemCount: 11,
-                    itemBuilder: (BuildContext context, int index) {
-                      isActive.add(false);
-                      // return wat elke rij uit de scrollbar nodig heeft
-                      return Column(
-                        children: [
-                          // detect een klik op de module
-                          GestureDetector(
-                            onTap: (() => setState(() => isActive[index] = !isActive[index])
-                              ),
-                            child: Image.asset(
-                              isActive[index] ? 'assets/SmartPlugON.png' : 'assets/SmartPlugOFF.png',
-                              scale: 3,
-                            ),
-                            // plaatje smart plug
-                          ),
-                          // naam module
-                          Padding(
-                            padding: const EdgeInsets.all(1.0),
-                            child: Text(
-                              'Module $index',
-                              style: const TextStyle(
-                                fontSize: 25.0,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ],
-                      );
-                    },
-                  ),
-                ),
+                  padding: EdgeInsets.only(top: 100.0),
+                  // scrollbar modules
+                  child: ModuleConstructor(),
               ),
             ),
           ],
         );
       },
+    );
+  }
+}
+
+class ModuleConstructor extends StatefulWidget {
+  const ModuleConstructor({Key? key}) : super(key: key);
+
+  @override
+  _ModuleConstructorState createState() => _ModuleConstructorState();
+}
+
+class _ModuleConstructorState extends State<ModuleConstructor> {
+  List<bool> isActive = [];
+
+  @override
+  Widget build(BuildContext context) {
+    final ScrollController _firstController = ScrollController();
+
+    return Scrollbar(
+      controller: _firstController,
+      child: ListView.builder(
+        controller: _firstController,
+        itemCount: 11,
+        itemBuilder: (BuildContext context, int index) {
+          isActive.add(false);
+          // return wat elke rij uit de scrollbar nodig heeft
+          return Column(
+            children: [
+              // detect een klik op de module
+              GestureDetector(
+                onTap: (() => setState(() => isActive[index] = !isActive[index])
+                ),
+                child: Image.asset(
+                  isActive[index] ? 'assets/SmartPlugON.png' : 'assets/SmartPlugOFF.png',
+                  scale: 3,
+                ),
+                // plaatje smart plug
+              ),
+              // naam module
+              Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: Text(
+                  'Module $index',
+                  style: const TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
