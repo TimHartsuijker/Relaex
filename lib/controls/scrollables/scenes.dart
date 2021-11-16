fc002c741d3a9acfa990a595aa16596d8dfebc2e
Sven Vos <sven.vos@omnilan.eu>
2021-11-16 09:04:38 +0100

    Kan nu maar een scene tergelijk aangezet worden

--- lib/controls/scrollables/scenes.dart
+++ lib/controls/scrollables/scenes.dart
@@ -1,56 +1,70 @@
 import 'package:flutter/material.dart';
 
-class Scenes extends StatefulWidget {
-  const Scenes({Key? key}) : super(key: key);
-
-  @override
-  _ScenesState createState() => _ScenesState();
-}
-
-class _ScenesState extends State<Scenes> {
-  List<bool> isActive = [];
+class Scenes extends StatelessWidget {
+  const Scenes ({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
-    final ScrollController _firstController = ScrollController();
-
     return Row(
-      children: <Widget>[
+      children: const <Widget>[
         // wit ruimte
         SizedBox(
           height: 955,
           width: 375.0,
           child: Padding(
-            padding: const EdgeInsets.only(top: 100.0),
+            padding: EdgeInsets.only(top: 100.0),
             // scrollbar scenes
-            child: Scrollbar(
-              controller: _firstController,
-              child: ListView.builder(
-                  controller: _firstController,
-                  itemCount: 11,
-                  itemBuilder: (BuildContext context, int index) {
-                    isActive.add(false);
-                    // return wat elke rij uit de scrollbar nodig heeft
-                    return GestureDetector(
-                      onTap: () => setState(() => isActive[index] = !isActive[index]),
-                      child: Padding(
-                        padding: const EdgeInsets.all(25.0),
-                        // naam scene
-                        child: Text(
-                          'Scene $index',
-                          style: TextStyle(
-                            fontSize: 40.0,
-                            color: isActive[index] ? Colors.blue : Colors.white,
-                          ),
-                        ),
-                      ),
-                    );
-                  }),
-            ),
+            child: SceneModule(),
           ),
         ),
       ],
+    );
+  }
+}
+
+class SceneModule extends StatefulWidget {
+  const SceneModule({Key? key}) : super(key: key);
+
+  @override
+  _SceneModuleState createState() => _SceneModuleState();
+}
+
+class _SceneModuleState extends State<SceneModule> {
+  List<bool> isActive = [];
+
+  @override
+  Widget build(BuildContext context) {
+    final ScrollController _firstController = ScrollController();
+
+    return Scrollbar(
+      controller: _firstController,
+      child: ListView.builder(
+          controller: _firstController,
+          itemCount: 11,
+          itemBuilder: (BuildContext context, int index) {
+            isActive.add(false);
+            // return wat elke rij uit de scrollbar nodig heeft
+            return GestureDetector(
+              onTap: () => setState(() {
+                if (isActive.any((element) => element == true)) {
+                  isActive[isActive.indexWhere((element) => element == true)] = !isActive[isActive.indexWhere((element) => element == true)];
+                }
+
+                isActive[index] = !isActive[index];
+              }),
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                // naam scene
+                child: Text(
+                  'Scene $index',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: isActive[index] ? Colors.blue : Colors.white,
+                  ),
+                ),
+              ),
+            );
+          }),
     );
   }
 }
