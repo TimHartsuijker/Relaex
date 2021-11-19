b166e290237c1b678c03196d8fbdb2335c00ea0d
Sven Vos <sven.vos@omnilan.eu>
2021-11-19 08:33:36 +0100

    Added more OOP to 'scenes'

--- lib/controls/objects/scenes/scenemanager.dart
+++ lib/controls/objects/scenes/scenemanager.dart
@@ -1,11 +1,11 @@
 import 'package:relaex/controls/objects/scenes/scenebo.dart';
 
 class SceneManager {
-  static List<SceneBO> scenes = [];
+  static List<SceneConstructor> scenes = [];
 
  static loadScenes(){
     for(int s = 0; s <= 30; s++){
-      SceneBO scene = SceneBO(s, 'scene $s');
+      SceneConstructor scene = SceneConstructor(sceneBO: SceneBO(s, 'Scene $s'));
       scenes.add(scene);
     }
   }
--- lib/controls/scrollables/scenes.dart
+++ lib/controls/scrollables/scenes.dart
@@ -30,7 +30,6 @@ class SceneConstructor extends StatefulWidget {
 }
 
 class _SceneConstructorState extends State<SceneConstructor> {
-  List<bool> isActive = [];
 
   @override
   Widget build(BuildContext context) {
@@ -40,46 +39,10 @@ class _SceneConstructorState extends State<SceneConstructor> {
       controller: _firstController,
       child: ListView.builder(
         controller: _firstController,
-        itemCount: 30,
+        itemCount: SceneManager.scenes.length,
         itemBuilder: (BuildContext context, int index) {
-          isActive.add(false);
-          String title = SceneManager.scenes[index].title;
-          String description = SceneManager.scenes[index].description;
           // return wat elke rij uit de scrollbar nodig heeft
-          return GestureDetector(
-            onTap: () => setState(() {
-              /*if (isActive.any((element) => element == true && element != isActive[index])) {
-                  isActive[isActive.indexWhere((element) => element == true)] = !isActive[isActive.indexWhere((element) => element == true)];
-                }*/
-
-              isActive[index] = !isActive[index];
-            }),
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 25.0, right: 70.0),
-              // naam scene
-              child: Column(
-                children: [
-                  Padding(padding: const EdgeInsets.only(right: 120.0),
-                  child: Text(
-                    title,
-                    style: TextStyle(
-                      fontSize: 40.0,
-                      color: isActive[index] ? Colors.blue : Colors.white,
-                    ),
-                  ),
-                  ),
-
-                  Text(
-                    description,
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      color: isActive[index] ? Colors.blue : Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
+          return SceneManager.scenes[index];
         },
       ),
     );
